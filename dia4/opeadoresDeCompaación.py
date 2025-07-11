e2c3948ff6b814fdf7a9c6e9ad0a71cb2364f076
grilosystems <mangopu@gmail.com>
2025-07-11 01:44:06 -0600

    documentacion

---
+++ dia4/opeadoresDeCompaación.py
@@ -0,0 +1,21 @@
+import math
+mi_variable = 10==25
+
+print(mi_variable)
+
+mi_variable = 'blanco'=='Blanco'.lower()
+
+print(mi_variable)
+
+mi_variable = 100.0==100
+
+print(mi_variable)
+
+num1 = math.sqrt(25)
+num2 = 5
+
+mi_bool = num1 == num2
+print(mi_bool)
+
+mi_bool = 4 < 5 < 6
+print(mi_bool)
