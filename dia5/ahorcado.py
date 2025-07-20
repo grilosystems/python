3bc216893471aa9e397506a1dbb2d94e405a7d61
grilosystems <mangopu@gmail.com>
2025-07-19 22:10:48 -0600

    inicio del game ahorcado

---
+++ dia5/ahorcado.py
@@ -0,0 +1,92 @@
+'''
+Juego del ahorcado
+'''
+
+from random import *
+
+# elegir palabra y mostrar guiones, cargar vidas
+def inicio_ahorcado():
+    estados_mexico = [
+    "Aguascalientes", "Baja California", "Baja California Sur", 
+    "Campeche", "Chiapas", "Chihuahua", "Coahuila", "Colima", 
+    "CDMX", "Durango", "Guanajuato", "Guerrero", 
+    "Hidalgo", "Jalisco", "Mexico", "Michoacan", "Morelos", 
+    "Nayarit", "Nuevo Leon", "Oaxaca", "Puebla", "Queretaro", 
+    "Quintana Roo", "San Luis Potosi", "Sinaloa", "Sonora", 
+    "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatan", 
+    "Zacatecas"]
+    
+    palabra_secreta = choice(estados_mexico)
+    guiones = '-' * len(palabra_secreta)
+    
+    parametros_inicio = {"vidas":6, "estado":palabra_secreta.lower(), "guiones":guiones}
+    
+    return parametros_inicio
+
+def obtener_una_letra(mensaje="Ingresa una letra: "):
+    while True:
+        entrada = input(mensaje).strip()
+        if not(len(entrada) == 1 or entrada.isspace()):
+            print("Debe ser exactamente un carácter.")
+        elif not (entrada.isalpha() or entrada.isspace()):
+            print("Solo se permiten letras (a-z) minus o espacio.")
+        else:
+            return entrada.lower()
+
+def enmascarar_palabra(palabra, letras_reveladas=None):
+    if letras_reveladas is None:
+        letras_reveladas = set()
+    
+    resultado = []
+    for letra in palabra:
+        if letra.lower() in letras_reveladas or letra.upper() in letras_reveladas:
+            resultado.append(letra)
+        else:
+            resultado.append('_')
+    return ' '.join(resultado)
+
+'''
+    pedir letra al usuario, 
+    validar letra
+    buscar letra en la palabra secreta
+    
+    si la letra no se encuentra en la palabra secreta agregar a la lista de incorrectas
+        mostrar lista de la letras incorrectas
+        descontar una vida
+        mostrar vidas restantes
+        si ya no tiene vidas gameover
+        
+    si la letra se encuentra en la palabra secreta 
+        mostrar guines por letras correctas
+        se completo la palabra? jugador gana
+'''
+def play_game(config_inicio):
+    lista_errores = []
+    vidas = config_inicio['vidas']
+    guiones = config_inicio['guiones']
+    estado = config_inicio['estado']
+    letras_encontradas = []
+    
+    print("Este es el juego del ahorcado version estados de Mexico")
+    print(f"{estado} Adivina el estado de Mexico que pense tienes {vidas} vidas: ")
+    print(f"{guiones}")
+    while vidas > 0:
+        
+        letra = obtener_una_letra("Adivina la letra: ")
+        
+        if letra in letras_encontradas:
+            print(f"La letra {letra} ya ha sido encontrada")
+            continue
+        
+        letras_encontradas = enmascarar_palabra(letra, estado)
+        if letra in estado:
+            print(letras_encontradas)
+        else:
+            lista_errores.append(letra)
+            print(lista_errores)
+            vidas -= 1
+            print(f"Te quedan {vidas} vidas...")
+            
+    
+play_game(inicio_ahorcado())
+
